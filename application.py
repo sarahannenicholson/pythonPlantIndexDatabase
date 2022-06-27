dacdfd6d07ef39d23d508fdb84d2cf34ecfcbb72
Sarah-Anne Nicholson <SarahAnneNicholson@myop.ac.nz>
2022-06-27 15:58:50 -0400

    	Entered 27 Test entries into plant index database 	Updated code to read from new database 	Updated code to take plant_name from new database to catalogue page 	Update code to take plant_name to return plantinfo page

--- application.py
+++ application.py
@@ -1,11 +1,18 @@
 from flask import Flask, render_template, request
 import pymysql
 
+# instance identifer - plant-index-database
+# username - admin
+# pw - admin123
+# port - 3306
+# hostname - plant-index-database.cyaimo2g0lsu.us-east-2.rds.amazonaws.com
+
 
 # creating the flask application
 application = Flask(__name__)
 
-db = pymysql.connect(host='plant-index-database.cyaimo2g0lsu.us-east-2.rds.amazonaws.com', user='admin', password='admin123')
+db = pymysql.connect(host='plant-index-database.cyaimo2g0lsu.us-east-2.rds.amazonaws.com', user='admin',
+                     password='admin123')
 
 cursor = db.cursor()
 
@@ -42,9 +49,15 @@ def links():
 
 
 # filterable catalog page
+# takes data from the selected table ('use ...') and parses
+# with the .execute command to display as list on the page
 @application.route('/catalog')
 def catalog():
-    return render_template('catalog.html')
+    sql = 'use plant_database'
+    cursor.execute(sql)
+    cursor.execute("SELECT plant_name FROM plant_information")
+    data = cursor.fetchall()
+    return render_template('catalog.html', data=data)
 
 
 # search page
@@ -67,12 +80,23 @@ def calendar():
 
 # plant info page
 # to link to individual plant item pages
-@application.route('/plantinfo')
-def plantinfo():
-    return render_template('plantinfo.html')
+# currently works when entering the url manually
+# does not work by clicking the link in the catalog page
+@application.route('/plantinfo/<plant_name>', methods=['GET', 'POST'])
+def plantinfo(plant_name):
+    sql = 'use plant_database'
+    cursor.execute(sql)
+    sql2 = 'SELECT * FROM plant_information WHERE plant_name = plant_name'
+    cursor.execute(sql2)
+
+    if plant_name in 'testing':
+        return 'success'
+
+    return render_template('plantinfo.html', plant_name=plant_name)
 
 
 # testing page to make sure the db connection is working
+# currently uses secondary template to add names to a random table on the database to check import works
 @application.route('/testing', methods=['GET', 'POST'])
 def testing():
     if request.method == "POST":
@@ -83,7 +107,6 @@ def testing():
         lastName = details['lname']
         cursor.execute("INSERT INTO MyUsers(firstName, lastName) VALUES (%s, %s)", (firstName, lastName))
         cursor.connection.commit()
-        cursor.close()
         return 'success'
     return render_template('testing.html')
 
